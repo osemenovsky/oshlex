2bff55a5d015327202d26ff949b4724a9ba5229f
Oleg Semenovskiy <o.semenovskiy@qiwi.ru>
2016-07-13 17:20:40 +0300

    added setup.py and usage examples

---
+++ examples/example.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+from oshlex.config import Rule, Configuration
+from oshlex import handlers
+
+# Defining rules
+root = Rule('root')
+server = Rule('server')
+host = Rule('host', handler=handlers.text, unique=True, mandatory=True)
+port = Rule('port', handler=handlers.integer, unique=True, mandatory=True)
+user = Rule('user', unique=False, mandatory=False)
+username = Rule('username', handler=handlers.text, unique=True, mandatory=True)
+password = Rule('password', handler=handlers.text, unique=True, mandatory=True)
+
+# Chaining rules together
+user.add(username)
+user.add(password)
+server.add(host)
+server.add(port)
+server.add(user)
+root.add(server)
+
+conf = Configuration(root, './example.conf')
+conf.read()
+
+print(conf)
---
+++ setup.py
@@ -0,0 +1,10 @@
+import os
+from setuptools import setup
+
+setup(
+    name='oshlex',
+    version='0.0.1a',
+    packages=['oshlex'],
+    author='Oleg Semenovsky',
+    author_email='o.semenovsky@gmail.com',
+)
